129b661d7be3a02348b9d2d733f0c86da15d2b35
Mateusz <mateuszd2411@gmail.com>
2020-04-14 20:31:06 +0200

    I added floatingButton (Add button) in HomeActivity and change start activity to HomeActivity. I added material gradle and update gradle for firebase

--- app/src/main/java/com/mat/firebase/HomeActivity.java
+++ app/src/main/java/com/mat/firebase/HomeActivity.java
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomeActivity extends AppCompatActivity {
 
     private EditText inputSearch;
     private RecyclerView recyclerView;
+    private FloatingActionButton floatingBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +24,17 @@ public class HomeActivity extends AppCompatActivity {
 
         inputSearch = findViewById(R.id.inputSearch);
         recyclerView = findViewById(R.id.recyclerView);
+        floatingBtn = findViewById(R.id.floatingbtn);
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         recyclerView.setHasFixedSize(true);
+
+        floatingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+
 
     }
 }
