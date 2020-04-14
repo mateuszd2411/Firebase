05143f8ea8568b381b0ea4b0c9c0b372c5308e7a
Mateusz <mateuszd2411@gmail.com>
2020-04-14 20:49:17 +0200

    I added MyViewHolder. I started with LoadData in HomeActivity

--- app/src/main/java/com/mat/firebase/HomeActivity.java
+++ app/src/main/java/com/mat/firebase/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.mat.firebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -7,21 +8,30 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class HomeActivity extends AppCompatActivity {
 
     private EditText inputSearch;
     private RecyclerView recyclerView;
     private FloatingActionButton floatingBtn;
+    private FirebaseRecyclerOptions<CarModel> options;
+    private FirebaseRecyclerAdapter<CarModel,MyViewHolder> adapter;
+    private DatabaseReference dataRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        dataRef = FirebaseDatabase.getInstance().getReference().child("Car");
         inputSearch = findViewById(R.id.inputSearch);
         recyclerView = findViewById(R.id.recyclerView);
         floatingBtn = findViewById(R.id.floatingbtn);
@@ -35,6 +45,25 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+        LoadData();
+
+    }
+
+    private void LoadData() {
+
+        options = new FirebaseRecyclerOptions.Builder<CarModel>().setQuery(dataRef,CarModel.class).build();
+        adapter = new FirebaseRecyclerAdapter<CarModel, MyViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i, @NonNull CarModel carModel) {
+
+            }
+
+            @NonNull
+            @Override
+            public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return null;
+            }
+        };
 
     }
 }
---
+++ app/src/main/java/com/mat/firebase/MyViewHolder.java
@@ -0,0 +1,12 @@
+package com.mat.firebase;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+class MyViewHolder extends RecyclerView.ViewHolder {
+    public MyViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
