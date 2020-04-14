84491389169b42904ecd42bc86e1ae7e019add91
Mateusz <mateuszd2411@gmail.com>
2020-04-14 21:05:07 +0200

    I added Picasso library and load pictures from FireStore

--- app/src/main/java/com/mat/firebase/HomeActivity.java
+++ app/src/main/java/com/mat/firebase/HomeActivity.java
@@ -7,15 +7,18 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -54,16 +57,22 @@ public class HomeActivity extends AppCompatActivity {
         options = new FirebaseRecyclerOptions.Builder<CarModel>().setQuery(dataRef,CarModel.class).build();
         adapter = new FirebaseRecyclerAdapter<CarModel, MyViewHolder>(options) {
             @Override
-            protected void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i, @NonNull CarModel carModel) {
+            protected void onBindViewHolder(@NonNull MyViewHolder holder, int position, @NonNull CarModel carModel) {
 
+                holder.textView.setText(carModel.getCarName());
+                Picasso.get().load(carModel.getImageUrl()).into(holder.imageView);
             }
 
             @NonNull
             @Override
             public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                return null;
+
+                View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.single_view,parent,false);
+                return new MyViewHolder(v);
             }
         };
+        adapter.startListening();
+        recyclerView.setAdapter(adapter);
 
     }
 }
