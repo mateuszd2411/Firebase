841de48f60d8a902ab4feee635ca16a8f96283ab
Mateusz <mateuszd2411@gmail.com>
2020-04-14 16:14:14 +0200

    I added simply button mainActivity

--- app/src/main/java/com/mat/firebase/MainActivity.java
+++ app/src/main/java/com/mat/firebase/MainActivity.java
@@ -3,12 +3,26 @@ package com.mat.firebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnave = findViewById(R.id.btn);
+
+        btnave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }
