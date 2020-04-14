c62f34bd968c2f4b08f0404bc0df5887e6d3b8c7
Mateusz <mateuszd2411@gmail.com>
2020-04-14 20:08:20 +0200

    I added single_view images design

--- app/src/main/java/com/mat/firebase/HomeActivity.java
+++ app/src/main/java/com/mat/firebase/HomeActivity.java
@@ -1,14 +1,26 @@
 package com.mat.firebase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private EditText inputSearch;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        inputSearch = findViewById(R.id.inputSearch);
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.setHasFixedSize(true);
+
     }
 }
