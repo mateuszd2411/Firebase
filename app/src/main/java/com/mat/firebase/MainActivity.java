4662b8477602c3b55fbd0e71f08628fbabb3afd1
Mateusz <mateuszd2411@gmail.com>
2020-04-14 18:38:35 +0200

    I cleared project and added Realtime Database Firebase

--- app/src/main/java/com/mat/firebase/MainActivity.java
+++ app/src/main/java/com/mat/firebase/MainActivity.java
@@ -28,61 +28,13 @@ import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnave;
-    private static final int REQUEST_CODE_IMAGE = 101;
-    StorageReference ref;
-    ProgressBar progressBar;
-    TextView textViewProgress;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnave = findViewById(R.id.btn);
-        progressBar = findViewById(R.id.progressBar);
-        textViewProgress = findViewById(R.id.textProgress);
 
-        progressBar.setVisibility(View.GONE);
-        textViewProgress.setVisibility(View.GONE);
-
-        ref = FirebaseStorage.getInstance().getReference().child("Images");
-        btnave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(intent,REQUEST_CODE_IMAGE);
-            }
-        });
 
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE_IMAGE && data != null){
 
-            progressBar.setVisibility(View.VISIBLE);
-            textViewProgress.setVisibility(View.VISIBLE);
-
-            Uri uri = data.getData();
-            ref.child("image.jpg").putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    Toast.makeText(MainActivity.this, "Image successfully uploaded", Toast.LENGTH_SHORT).show();
-                    progressBar.setProgress(0);
-                    textViewProgress.setText("Uploaded 100%");
-                }
-            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-                    double progress = (100 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
-                    progressBar.setProgress((int) progress);
-                    textViewProgress.setText(progress + " %");
-                }
-            });
-
-        }
-    }
 }
