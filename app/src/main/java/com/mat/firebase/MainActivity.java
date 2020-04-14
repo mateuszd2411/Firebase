aaad33eb6f9c2678e576a2e8c5c67b645708382f
Mateusz <mateuszd2411@gmail.com>
2020-04-14 16:25:33 +0200

    I created the "Document" folder in Storege and placed a text file in it.

--- app/src/main/java/com/mat/firebase/MainActivity.java
+++ app/src/main/java/com/mat/firebase/MainActivity.java
@@ -1,14 +1,23 @@
 package com.mat.firebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnave;
+    StorageReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +25,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btnave = findViewById(R.id.btn);
 
+        reference = FirebaseStorage.getInstance().getReference().child("Document");
+
         btnave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                String data = "This is my data";
+                reference.child("file.txt").putBytes(data.getBytes()).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(MainActivity.this, "File upload Successfully!", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
 
