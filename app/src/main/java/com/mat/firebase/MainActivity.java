1cd2282156de717d1264e454527b92c5ff434cc3
Mateusz <mateuszd2411@gmail.com>
2020-04-14 17:28:20 +0200

    I added progressBar to upload Image

--- app/src/main/java/com/mat/firebase/MainActivity.java
+++ app/src/main/java/com/mat/firebase/MainActivity.java
@@ -11,11 +11,14 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
@@ -28,12 +31,19 @@ public class MainActivity extends AppCompatActivity {
     private Button btnave;
     private static final int REQUEST_CODE_IMAGE = 101;
     StorageReference ref;
+    ProgressBar progressBar;
+    TextView textViewProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnave = findViewById(R.id.btn);
+        progressBar = findViewById(R.id.progressBar);
+        textViewProgress = findViewById(R.id.textProgress);
+
+        progressBar.setVisibility(View.GONE);
+        textViewProgress.setVisibility(View.GONE);
 
         ref = FirebaseStorage.getInstance().getReference().child("Images");
         btnave.setOnClickListener(new View.OnClickListener() {
@@ -53,11 +63,23 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_CODE_IMAGE && data != null){
 
+            progressBar.setVisibility(View.VISIBLE);
+            textViewProgress.setVisibility(View.VISIBLE);
+
             Uri uri = data.getData();
             ref.child("image.jpg").putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                 @Override
                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                     Toast.makeText(MainActivity.this, "Image successfully uploaded", Toast.LENGTH_SHORT).show();
+                    progressBar.setProgress(0);
+                    textViewProgress.setText("Uploaded 100%");
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                    progressBar.setProgress((int) progress);
+                    textViewProgress.setText(progress + " %");
                 }
             });
 
