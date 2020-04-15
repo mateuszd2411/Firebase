a8f87a0a28624f5e1ff5ab7c9451938265a5ed5d
Mateusz <mateuszd2411@gmail.com>
2020-04-15 19:14:16 +0200

    I added Toolbar to EditNote and set text from note to editNote(now I can edit my notes)

--- app/src/main/java/com/mat/firenote/EditNote.java
+++ app/src/main/java/com/mat/firenote/EditNote.java
@@ -1,22 +1,34 @@
 package com.mat.firenote;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class EditNote extends AppCompatActivity {
 
     Intent data;
+    EditText editNoteTitle,editNoteContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_note);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         data = getIntent();
+
+        editNoteContent = findViewById(R.id.editNoteContent);
+        editNoteTitle = findViewById(R.id.editNoteTitle);
 
         String noteTitle = data.getStringExtra("title");
         String noteContent = data.getStringExtra("content");
+
+        editNoteTitle.setText(noteTitle);
+        editNoteContent.setText(noteContent);
     }
 }
