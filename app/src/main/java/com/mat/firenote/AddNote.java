d1c842acb04c238291e288249c8fe34d8004a9dd
Mateusz <mateuszd2411@gmail.com>
2020-04-15 17:28:47 +0200

    I added saving note to cloud firestore

--- app/src/main/java/com/mat/firenote/AddNote.java
+++ app/src/main/java/com/mat/firenote/AddNote.java
@@ -2,19 +2,28 @@ package com.mat.firenote;
 
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddNote extends AppCompatActivity {
 
     FirebaseFirestore fStore;
+    EditText noteTitle,noteContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +33,44 @@ public class AddNote extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         fStore = FirebaseFirestore.getInstance();
-        
+        noteContent = findViewById(R.id.addNoteContent);
+        noteTitle = findViewById(R.id.addNoteTitle);
+
         
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(AddNote.this, "Save btn is clicked", Toast.LENGTH_SHORT).show();
+                String nTitle = noteTitle.getText().toString();
+                String nContent = noteContent.getText().toString();
+
+                if (nTitle.isEmpty() || nContent.isEmpty()){
+                    Toast.makeText(AddNote.this, "Can not Save note with Empty Field", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                ///save note
+
+                DocumentReference docref = fStore.collection("notes").document();
+                Map<String,Object> note = new HashMap<>();
+                note.put("title",nTitle);
+                note.put("content",nContent);
+
+                docref.set(note).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(AddNote.this, "Note Added", Toast.LENGTH_SHORT).show();
+                        onBackPressed();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(AddNote.this, "Error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
