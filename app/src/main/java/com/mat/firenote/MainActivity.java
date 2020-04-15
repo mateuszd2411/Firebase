8ad7e7b3775dd4c9eae5df3cd65098b3225c4655
Mateusz <mateuszd2411@gmail.com>
2020-04-15 12:23:12 +0200

    I added the option to click in layoutDrawer

--- app/src/main/java/com/mat/firenote/MainActivity.java
+++ app/src/main/java/com/mat/firenote/MainActivity.java
@@ -1,15 +1,18 @@
 package com.mat.firenote;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  NavigationView.OnNavigationItemSelectedListener{
 
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle toggle;
@@ -25,9 +28,19 @@ public class MainActivity extends AppCompatActivity {
 
         drawerLayout = findViewById(R.id.drawer);
         nav_view = findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(this);
         toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
         drawerLayout.addDrawerListener(toggle);
         toggle.setDrawerIndicatorEnabled(true);
         toggle.syncState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            default:
+                Toast.makeText(this, "Coming soon.", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
