5393425300b68cf9ff5a47a9afcb81e048900117
Mateusz <mateuszd2411@gmail.com>
2020-04-15 18:33:16 +0200

    I added Noteadapter a copied from main adapter and change 'rigid rules' to getters. Corrected firebase dependence in gradle

--- app/src/main/java/com/mat/firenote/MainActivity.java
+++ app/src/main/java/com/mat/firenote/MainActivity.java
@@ -4,26 +4,34 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.mat.firenote.model.Adapter;
+import com.mat.firenote.model.Note;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements  NavigationView.OnNavigationItemSelectedListener{
 
@@ -33,6 +41,7 @@ public class MainActivity extends AppCompatActivity implements  NavigationView.O
     RecyclerView noteList;
     Adapter adapter;
     FirebaseFirestore fStore;
+    FirestoreRecyclerAdapter<Note,NoteViewHolder> noteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +55,37 @@ public class MainActivity extends AppCompatActivity implements  NavigationView.O
 
         Query query = fStore.collection("notes").orderBy("title",Query.Direction.DESCENDING);
 
+        FirestoreRecyclerOptions<Note> allNotes = new FirestoreRecyclerOptions.Builder<Note>()
+                .setQuery(query,Note.class)
+                .build();
 
+        noteAdapter = new FirestoreRecyclerAdapter<Note, NoteViewHolder>(allNotes) {
+            @Override
+            protected void onBindViewHolder(@NonNull NoteViewHolder noteViewHolder, int i, @NonNull final Note note) {
+                noteViewHolder.noteTitle.setText(note.getTitle());
+                noteViewHolder.noteContent.setText(note.getContent());
+                final int code = getRandomColor();
+                noteViewHolder.mCardView.setCardBackgroundColor(noteViewHolder.view.getResources().getColor(code,null));
+
+                noteViewHolder.view.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent i = new Intent(view.getContext(), NoteDetails.class);
+                        i.putExtra("title",note.getTitle());
+                        i.putExtra("content",note.getContent());
+                        i.putExtra("code",code);
+                        view.getContext().startActivity(i);
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_view_layout,parent,false);
+                return new NoteViewHolder(view);
+            }
+        };
 
         noteList = findViewById(R.id.notelist);
 
@@ -112,5 +151,40 @@ public class MainActivity extends AppCompatActivity implements  NavigationView.O
             Toast.makeText(this, "Settings Menu is Clicked", Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public class NoteViewHolder extends  RecyclerView.ViewHolder{
+
+        TextView noteTitle,noteContent;
+        View view;
+        CardView mCardView;
+
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            noteTitle = itemView.findViewById(R.id.titles);
+            noteContent = itemView.findViewById(R.id.content);
+            mCardView = itemView.findViewById(R.id.noteCard);
+            view = itemView; ///for click
+        }
+    }
+
+    private int getRandomColor() {
+
+        List<Integer> colorCode = new ArrayList<>();
+        colorCode.add(R.color.blue);
+        colorCode.add(R.color.yellow);
+        colorCode.add(R.color.skyblue);
+        colorCode.add(R.color.lightPurple);
+        colorCode.add(R.color.lightGreen);
+        colorCode.add(R.color.gray);
+        colorCode.add(R.color.pink);
+        colorCode.add(R.color.red);
+        colorCode.add(R.color.greenlight);
+        colorCode.add(R.color.notgreen);
+
+        Random randomColor = new Random();
+        int number = randomColor.nextInt(colorCode.size());
+        return  colorCode.get(number);
+
     }
 }
