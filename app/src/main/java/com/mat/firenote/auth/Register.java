cd84cf51c4b6928a5d61a99cd6c1adb027291576
Mateusz <mateuszd2411@gmail.com>
2020-04-16 12:29:54 +0200

    I added back icon and title to action bar in Register activity

--- app/src/main/java/com/mat/firenote/auth/Register.java
+++ app/src/main/java/com/mat/firenote/auth/Register.java
@@ -1,9 +1,13 @@
 package com.mat.firenote.auth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.mat.firenote.MainActivity;
 import com.mat.firenote.R;
 
 public class Register extends AppCompatActivity {
@@ -12,5 +16,15 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        getSupportActionBar().setTitle("Create New Account");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+        return super.onOptionsItemSelected(item);
     }
 }
