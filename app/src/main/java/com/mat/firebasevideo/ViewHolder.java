90a6ae80b6a0f3611fdb61cf300b36f3403d4015
Mateusz <mateuszd2411@gmail.com>
2020-04-23 16:23:59 +0200

    I added Member.java and set constructors and getters setters. Added row.xml (container for exoplayer and title)

---
+++ app/src/main/java/com/mat/firebasevideo/ViewHolder.java
@@ -0,0 +1,16 @@
+package com.mat.firebasevideo;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+
+    View mView;
+
+
+    public ViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
