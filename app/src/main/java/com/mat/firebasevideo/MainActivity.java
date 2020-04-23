2fb96c8a51df1308a36336e168df697c6d4abb0a
Mateusz <mateuszd2411@gmail.com>
2020-04-23 18:19:25 +0200

    sw

--- app/src/main/java/com/mat/firebasevideo/MainActivity.java
+++ app/src/main/java/com/mat/firebasevideo/MainActivity.java
@@ -1,18 +1,57 @@
 package com.mat.firebasevideo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView mRecyclerView;
+    FirebaseDatabase database;
+    DatabaseReference reference;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.recyclerviewVideo);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("video");
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+
+
+        FirebaseRecyclerAdapter<Member,ViewHolder> firebaseRecyclerAdapter =
+                new FirebaseRecyclerAdapter<Member, ViewHolder>(
+                        Member.class,
+                        R.layout.row,
+                        ViewHolder.class,
+                        reference
+                ) {
+                    @Override
+                    protected void populateViewHolder(ViewHolder viewHolder, Member member, int i) {
+                        viewHolder.setVideo(getApplication(),member.getTitle(),member.getUrl());
+                    }
+                };
+        mRecyclerView.setAdapter(firebaseRecyclerAdapter);
     }
 }
